04b7f75b7447e31974a22870ec820ab20464ca8d
John Hyland <jhyland@damballa.com>
2010-09-04 10:54:48 -0400

    balance handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :fulfilled_orders, :class_name => 'Order', :foreign_key => 'fulfiller_id'
+  has_many :placed_orders, :class_name => 'Order', :foreign_key => 'orderer_id'
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
@@ -6,4 +9,15 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  scope :with_positive_balance, joins(:fulfilled_orders).joins(:placed_orders).where('sum(fulfilled_orders.cost) > sum(placed_orders.cost)')
+
+  # Shows the net total owed to this user.  A negative balance indicates that
+  # this user owes money.
+  #
+  def balance
+    # TODO This will eventually get unwieldy, but I'm implementing the simplest
+    # thing that will work for now.  Refactor when scaling becomes an issue.
+    Order.find_all_by_fulfiller_id(self.id).collect(&:cost).sum - Order.fulfilled.where(:orderer => self).collect(&:cost).sum
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,43 @@
 require 'spec_helper'
 
 describe User do
+  before :all do
+    @alice = User.make
+    @bob = User.make
+    @charlie = User.make
+
+    @basic   = Item.make :name => "Basic Burrito",   :cost => 5
+    @awesome = Item.make :name => "Awesome Burrito", :cost => 7
+
+    Order.make(:orderer => @bob,   :fulfiller => @alice  ).add_item @awesome
+    Order.make(:orderer => @alice, :fulfiller => @charlie).add_item @basic
+    Order.make(:orderer => @alice, :fulfiller => @charlie).add_item @basic
+    Order.make(:orderer => @alice, :fulfiller => nil).add_item @basic
+  end
+
+  context '#balance' do
+    it "should show the net amount owed to/by the user" do
+      @alice.balance.should == @awesome.cost - @basic.cost - @basic.cost
+      @bob.balance.should == 0 - @awesome.cost
+      @charlie.balance.should == @basic.cost + @basic.cost
+    end
+  end
+
+  context '.with_positive_balance' do
+    it "should return all users with a positive balance" do
+      pending "track cost of orders"
+      users = User.with_positive_balance
+      users.should have(1).users
+      users.should include(@charlie)
+    end
+  end
+
+  context '.with_negative_balance' do
+    it 'should return all users with a negative balance' do
+      pending "track cost of orders"
+      users = User.with_negative_balance
+      users.should have(2).users
+      users.should include(@alice, @bob)
+    end
+  end
 end
