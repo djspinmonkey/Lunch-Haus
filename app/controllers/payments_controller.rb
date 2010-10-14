8ba5f3cd6f9afcdd2ebf7962c57ff04881caa7d7
Emily Price <price.emily@gmail.com>
2010-10-13 22:36:37 -0400

    Add authentication to payments and some restaurants actions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,6 @@
 class PaymentsController < ApplicationController
+  before_filter :authenticate_user!
+
   # GET /payments
   # GET /payments.xml
   def index
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
   before_filter { @title = { :name => 'Restaurants', :link => restaurants_path } }
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   # GET /restaurants
   # GET /restaurants.xml
