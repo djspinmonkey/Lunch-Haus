22cb57c5c05e96961201ae77e1c4a52b41067718
Emily Price <price.emily@gmail.com>
2012-03-16 12:28:19 -0400

    Authorize UsersController, only index and show used
    Remove the other routes since that's handled better by Devise

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+
+    can :read, User
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
