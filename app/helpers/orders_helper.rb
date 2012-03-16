89d1fa61ef7e6812b499022ebce4781dd8f7a8b9
Emily Price <price.emily@gmail.com>
2012-03-15 22:11:07 -0400

    Fix explosion on orders#index

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,10 @@
 module OrdersHelper
+  def restaurants( order )
+    restaurants = order.items.map { |item| item.restaurant.try(:name) }.compact.uniq
+    if restaurants.present?
+      restaurants.join(', ')
+    else
+      content_tag :em, "Unknown restaurant"
+    end
+  end
 end
