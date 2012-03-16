0c244b35ee2918fff01955cbbd18f14e1f74fe9c
John Hyland <jhyland@damballa.com>
2012-03-15 23:45:43 -0400

    add order_acceptances_controller

--- app/controllers/order_acceptances_controller.rb
+++ app/controllers/order_acceptances_controller.rb
@@ -1,11 +1,27 @@
 class OrderAcceptancesController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!  # TODO: fix specs to not asplode with this
 
   def create
-    # TODO
+    Order.transaction do
+      params[:order_acceptances].each do |acc|
+        order = Order.find acc[:order][:id]
+        user  = User.find  acc[:user ][:id]
+        order.accepter = user
+        order.save!
+      end
+      head :ok
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    render :json => [:errors => [e.message]], :status => 404
+  rescue StandardError => e
+    render :json => [:errors => [e.message]], :status => 500
   end
 
   def destroy
-    # TODO
+    order = Order.find params[:order_acceptance][:id]
+    order.accepter = nil
+    order.save!
+  rescue StandardError => e
+    render :json => [:errors => [e.message]], :status => 500
   end
 end
---
+++ spec/controllers/order_acceptances_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+describe OrderAcceptancesController do
+  describe "POST create" do
+    let(:order) { create :order }
+    let(:user)  { create :user }
+
+    before do
+      Order.stub(:find) { order }
+      User.stub(:find) { user }
+      post(:create, :order_acceptances => [{:order => {:id => order.id}, :user => {:id => user.id}}])
+    end
+
+    subject { response }
+
+    its(:status) { should == 200 }
+
+    it "should assign a fulfiller to an order" do
+      order.reload
+      order.accepter.should == user
+    end
+
+    context "with an unknown user" do
+      before do 
+        User.stub(:find) { raise ActiveRecord::RecordNotFound, "user not found" }
+        post(:create, :order_acceptances => [{:order => {:id => order.id}, :user => {:id => user.id}}])
+      end
+      subject { response }
+
+      its(:status) { should == 404 }
+      its(:body) { should include "user not found" }
+    end
+
+    context "with an unknown order" do
+      before do
+        Order.stub(:find) { raise ActiveRecord::RecordNotFound, "order not found" }
+        post(:create, :order_acceptances => [{:order => {:id => order.id}, :user => {:id => user.id}}])
+      end
+      subject { response }
+
+      its(:status) { should == 404 }
+      its(:body) { should include "order not found" }
+    end
+
+    context "when there is an internal error" do
+      before do
+        User.stub(:find) { raise "OH NOES" }
+        post(:create, :order_acceptances => [{:order => {:id => order.id}, :user => {:id => user.id}}])
+      end
+      subject { response }
+
+      its(:status) { should == 500 }
+      its(:body) { should include "OH NOES" }
+    end
+  end
+end
