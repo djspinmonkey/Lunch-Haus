12365c6c9f84750edcf41ab0b87d61680a39e37c
John Hyland <jhyland@damballa.com>
2012-03-15 23:09:45 -0400

    user order.restaurant instead of helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,10 +1,2 @@
 module OrdersHelper
-  def restaurants( order )
-    restaurants = order.items.map { |item| item.restaurant.try(:name) }.compact.uniq
-    if restaurants.present?
-      restaurants.join(', ')
-    else
-      content_tag :em, "Unknown restaurant"
-    end
-  end
 end
