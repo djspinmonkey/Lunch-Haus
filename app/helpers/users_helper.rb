3d031fc95ea0d7675bcedf725c9938a999f2f268
John Hyland <jhyland@damballa.com>
2012-03-20 15:48:28 -0400

    add gravatr_user() helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def gravatar_url(user)
+    # TODO: custom default avatar?
+    gravatar_hash = Digest::MD5.hexdigest(user.email.downcase)
+    "https://gravatar.com/avatar/#{gravatar_hash}"
+  end
+
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe UsersHelper do
+
+  describe '#gravatar_url' do
+    let(:user) { User.make :email => 'Bob@test.com' }
+    subject { gravatar_url(user) }
+    it { should == 'https://gravatar.com/avatar/ebf3a1ca1e3ca553e2bd873b3cd96390' }
+  end
+
 end
