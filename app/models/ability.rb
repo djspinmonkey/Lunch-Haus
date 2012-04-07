541a9bdb9c1b49db8cb0fa4d70d65fa1d9c4003c
John Hyland <jhyland@damballa.com>
2012-04-07 13:35:35 -0400

    fill in more permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,29 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     can :read, User
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    can :manage, User, :id => user.id
+
+    can :manage, Order, :orderer_id => user.id
+    can :accept, Order, :accepter_id => nil
+    can :unaccept, Order, :accepter_id => user.id
+
+    # TODO: These will need to be tightened once we have a little more of a lifecycle around payments.
+    can :manage, Payment, :payer_id => user.id
+    can :manage, Payment, :recipient_id => user.id
+
+    # TODO: Tighten these?
+    can :manage, Restaurant
+    can :manage, Item
   end
 end
